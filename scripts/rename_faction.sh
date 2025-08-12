499751abfe3685aa332cbd08b1e09bdd0c970b11
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-08-12 12:28:35 +0200

    Improve script

--- scripts/rename_faction.sh
+++ scripts/rename_faction.sh
@@ -14,15 +14,37 @@ if [[ "$PWD" =~ scripts$ ]]; then
     echo " "
     echo "Tip: like this"
     echo " "
-    echo "  ./scripts/fix_markdown_style_errors.sh"
+    echo "  ./scripts/rename_faction.sh xayid spawn"
     echo " "
     exit 42
 fi
 
+if [ "$#" -ne 2 ]; then
+    echo "Please use 2 arguments, for example: "
+    echo " "
+    echo "  ./scripts/rename_faction.sh xayid spawn"
+    echo " "
+    echo "Number of arguments used: $#"
+fi
+
+from_faction=$1
+to_faction=$2
+
+echo "From (lowercase): ${from_faction}"
+echo "To (lowercase): ${to_faction}"
+
+from_uppercase_faction=${from_faction^}
+to_uppercase_faction=${to_faction^}
+
+echo "From (uppercase): ${from_uppercase_faction}"
+echo "To (uppercase): ${to_uppercase_faction}"
+
+# Allows rename to use **
 # Thanks to pa4080, from
 # https://askubuntu.com/questions/1287359/how-to-use-the-command-rename-on-subdirectories-as-well#1287371
 shopt -s globstar
+rename --nono "s/${from_faction}/${to_faction}/" **/*
 
-rename --nono 's/xayid/spawn/' **/*
-rename --nono 's/genetron/kingdom/' **/*
-rename --nono 's/keiron/rooxx/' **/*
+#rename --nono 's/xayid/spawn/' **/*
+#rename --nono 's/genetron/kingdom/' **/*
+#rename --nono 's/keiron/rooxx/' **/*
