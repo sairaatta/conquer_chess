503f71cf7925bf89b4c2fafd43bbd7e5404524fd
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-08-12 13:33:27 +0200

    Add new logos

--- resources/textures/race_logos/convert_webps_to_pngs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-convert genetron_logo_185_x_185.webp genetron_logo_185_x_185.png
-convert keiron_logo_185_x_185.webp keiron_logo_185_x_185.png
-convert xayid_logo_185_x_185.webp xayid_logo_185_x_185.png
-
-convert -resize 16x16 genetron_logo_185_x_185.webp genetron_logo_16_x_16.png
-convert -resize 16x16 keiron_logo_185_x_185.webp keiron_logo_16_x_16.png
-convert -resize 16x16 xayid_logo_185_x_185.webp xayid_logo_16_x_16.png
