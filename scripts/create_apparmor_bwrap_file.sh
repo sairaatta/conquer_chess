e02bf9ac78b9fb06ea5d55844f9a5754aef2ac7b
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-08-13 12:02:00 +0200

    Fix 'bwrap: setting up uid map: Permission denied'

---
+++ scripts/create_apparmor_bwrap_file.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#
+# Creates a 
+#
+# From https://etbe.coker.com.au/2024/04/24/ubuntu-24-04-bubblewrap/
+#
+# Usage:
+#
+#   sudo create_apparmor_bwrap_file.sh
+# 
+
+
+echo "Modifying the file '/etc/apparmor.d/bwrap'"
+
+cat >/etc/apparmor.d/bwrap <<EOL
+
+abi <abi/4.0>,
+include <tunables/global>
+
+profile bwrap /usr/bin/bwrap flags=(unconfined) {
+  userns,
+
+  # Site-specific additions and overrides. See local/README for details.
+  include if exists <local/bwrap>
+}
+
+EOL
+
+cat /etc/apparmor.d/bwrap
+
+
+
