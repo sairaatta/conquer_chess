e2173c014e13624ac0fb3b42fd48fd85856ddac0
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-08-17 14:06:40 +0200

    Add check Steam install script

--- scripts/check_steam_install.sh
+++ scripts/check_steam_install.sh
@@ -2,4 +2,22 @@
 #
 # Check the Steam install
 #
-ls ~/.local/share/Steam/steamapps/common/conquer_chess
+# Usage:
+#
+
+cc_install_folder=~/.local/share/Steam/steamapps/common/conquer_chess
+
+echo "Conquer Chess Steam install folder: ${cc_install_folder}"
+echo "Content of Conquer Chess install folder:"
+ls ${cc_install_folder}
+
+if [ -d ${cc_install_folder}/EmptySteamDepot ]; then
+  echo "ERROR: Conquer Chess Steam install has no content."
+  exit 42
+fi
+
+if [ ! -f ${cc_install_folder}/conquer_chess ]; then
+  echo "ERROR: Conquer Chess executable is not found."
+  exit 43
+fi
+
